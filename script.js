64916075a357d212fc8d7391ff7cbd3deeb394db
Z-67 <zayn2830@gmail.com>
2023-11-13 11:11:48 +0000

    cant overwrite cells anymore

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ let isX = true;
 
 // Add a click event listener to each cell
 cells.forEach(cell => {
+    cell.clicked = false;
     cell.addEventListener('click', () => {
-        // Add 'X' or 'O' to cell when it's clicked
+      // Check if the cell has already been clicked
+      if (!cell.clicked) {
+        // Add 'X' or 'O' to the cell when it's clicked
         cell.textContent = isX ? 'X' : 'O';
         cell.style.color = 'black';  // Change color to black
-        cell.clicked = true;  // Add this line
-        // Switch the game state
+        cell.clicked = true;  
+        // Switch the game state for the next player's turn
         isX = !isX;
-    });
+    }
+});
 
     // Change hover effect based on game state
     cell.addEventListener('mouseover', () => {
         if (!cell.clicked) {
-            cell.textContent = isX ? 'X' : 'O';
-            cell.style.color = '#ccc';  // Add this line
+            cell.textContent = isX ? 'X' : 'O'; // Display 'X' or 'O' on hover
+            cell.style.color = '#ccc';  
         }
     });
 
