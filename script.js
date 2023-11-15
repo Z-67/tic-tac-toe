c3936548976ce7df0d12b3a20a8c262f7047fe85
Z-67 <zayn2830@gmail.com>
2023-11-15 10:56:36 +0000

    function to check for draw added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const cells = document.querySelectorAll('cell'); 
 const restartButton= document.querySelector('.restart')
+const winStatement = document.getElementById('winStatement');
 
-// Initialize game state so this is Xs turn
-let isX = true;
-let gameOver = false;
+
+let isX = true; // Initialize game state so this is Xs turn
+let gameOver = false; // used to not allow certain actions when game is over
 
 cells.forEach(cell => {
     cell.clicked = false;
@@ -13,23 +14,24 @@ cells.forEach(cell => {
             return; // Ignore clicks if the game is over or the cell has already been clicked
         }
       
-      if (!cell.clicked) {// if the cell has already been clicked, cant overwrite content
+      if (!cell.clicked) {// if the cell has already been clicked, wont allow it to overwrite content
         
-        cell.textContent = isX ? 'X' : 'O';// Add 'X' or 'O' to the cell when it's clicked
-        cell.style.color = 'black';  // Change color to black
+        cell.textContent = isX ? 'X' : 'O';//if isX is true, cell.textContent will be set to 'X' if isX is false, it will be set to 'O'
+        cell.style.color = 'black';  
         cell.clicked = true;  // so content stays in cell when mouseleaves
         
-        isX = !isX;// Switch the game state for the next player's turn
+        isX = !isX;// Switch the game state for the next player's turn after each click
         
         checkForWin();//check for win after each move
+        checkForDraw();
     }
 });
 
-    
+    //when hovering over cell
     cell.addEventListener('mouseover', () => {
-        if (!cell.clicked && !gameOver) {//if cell is not clicked and game is not over, show hover effect
-            cell.textContent = isX ? 'X' : 'O'; // Display X or O on hover
-            cell.style.color = '#ccc';  
+        if (!cell.clicked && !gameOver) {//if cell has not been clicked and game is not over, show hover effect
+            cell.textContent = isX ? 'X' : 'O'; // Display X or O on hover based on if isX=true 
+            cell.style.color = '#ccc'; // give grey colour
         }
     });
 
@@ -47,6 +49,7 @@ restartButton.addEventListener('click', () => {
         cell.style.color = ''; // Reset text color
         cell.clicked = false;
         cell.classList.remove('winning');
+        winStatement.textContent = '';
     });
 
     // Reset game state
@@ -55,8 +58,8 @@ restartButton.addEventListener('click', () => {
 });
 
 function checkForWin() {
-    // Define winning combinations (indices of cells in a row/column/diagonal)
-    const winningCombinations = [
+    // Define winning combinations 
+    let winningCombinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
         [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
         [0, 4, 8], [2, 4, 6]              // Diagonals
@@ -66,20 +69,42 @@ function checkForWin() {
     for (const combination of winningCombinations) {
         const [a, b, c] = combination;
 
-        // Check if the cells have the same content and are not empty
+        // Check if the wining combinations of cells have the same content
         if (cells[a].textContent !== '' &&
             cells[a].textContent === cells[b].textContent &&
             cells[a].textContent === cells[c].textContent) {
-            // Mark the winning cells with a line-through
+            // give cells winning class in CSS so they will have a line through
             cells[a].classList.add('winning');
             cells[b].classList.add('winning');
             cells[c].classList.add('winning');
 
-            // Display the win statement on the page   
-            const winStatement = document.getElementById('winStatement');
+            // end game when there is a winner
+            gameOver = true;
+
+            // Display the win statement on the page
             winStatement.textContent = `${cells[a].textContent} wins!`;
-            
-            gameOver=true;
+
         }
     }
+}
+
+function checkForDraw() {
+    // Check if all cells are clicked and check there is no winning combination by calling a function
+    const isDraw = Array.from(cells).every(cell => cell.clicked) && !checkWinningCombinations();
+
+    if (isDraw) {
+        winStatement.textContent = "It's a draw!";
+        gameOver = true; 
+    }
+}
+
+// function to check if there are winning combinations
+function checkWinningCombinations() {
+    // check every cell, if one them have a winning class added then the function will return true
+    for (const cell of cells) {
+        if (cell.classList.contains('winning')) {
+            return true;
+        }
+    }
+    return false;
 }
