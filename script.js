2582c8a4ca1067e27c017f9741b41af4dbf05b2d
Z-67 <zayn2830@gmail.com>
2023-11-15 08:45:20 +0000

    win function works

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const restartButton= document.querySelector('.restart')
 // Initialize game state so this is Xs turn
 let isX = true;
 
-// Add a click event listener to each cell
+
 cells.forEach(cell => {
     cell.clicked = false;
     cell.addEventListener('click', () => {
@@ -16,6 +16,8 @@ cells.forEach(cell => {
         cell.clicked = true;  // so content stays in cell when mouseleaves
         // Switch the game state for the next player's turn
         isX = !isX;
+        //check for win after each move
+        checkForWin();
     }
 });
 
@@ -29,7 +31,7 @@ cells.forEach(cell => {
 
     cell.addEventListener('mouseout', () => {
         if (!cell.clicked) {
-            cell.textContent = '';
+            cell.textContent = '';// when mouse leaves cell, hover effect will stop aswell
         }
     });
 });
@@ -40,8 +42,39 @@ restartButton.addEventListener('click', () => {
         cell.textContent = '';
         cell.style.color = ''; // Reset text color
         cell.clicked = false;
+        cell.classList.remove('winning');
     });
 
     // Reset game state
     isX = true;
 });
+
+function checkForWin() {
+    // Define winning combinations (indices of cells in a row/column/diagonal)
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+
+    // Check each winning combination
+    for (const combination of winningCombinations) {
+        const [a, b, c] = combination;
+
+        // Check if the cells have the same content and are not empty
+        if (cells[a].textContent !== '' &&
+            cells[a].textContent === cells[b].textContent &&
+            cells[a].textContent === cells[c].textContent) {
+            // Mark the winning cells with a line-through
+            cells[a].classList.add('winning');
+            cells[b].classList.add('winning');
+            cells[c].classList.add('winning');
+
+            // Display the win statement on the page   
+            const winStatement = document.getElementById('winStatement');
+            winStatement.textContent = `${cells[a].textContent} wins!`;
+            
+
+        }
+    }
+}
